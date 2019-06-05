795fd377cbfbb694e826c94b139debefd618f2af
abelsu7 <abel1995@163.com>
2019-06-05 22:58:01 +0800

    Add some comments in model/user.go

--- model/user.go
+++ model/user.go
@@ -11,13 +11,14 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-// User represents a registered user.
+// UserModel represents a registered user.
 type UserModel struct {
 	BaseModel
 	Username string `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"`
 	Password string `json:"password" gorm:"column:password;not null" binding:"required" validate:"min=5,max=128"`
 }
 
+// TableName returns the table name
 func (u *UserModel) TableName() string {
 	return "tb_users"
 }
